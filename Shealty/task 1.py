b4adbfc022178472d230cf53f367d8a28dcfa64a
Kwazzart <vladimirkwaz@yandex.ru>
2021-07-21 12:15:43 +0300

    кокс

--- Shealty/task 1.py
+++ Shealty/task 1.py
@@ -1,6 +1,12 @@
 #x=3
 #res=3**2
 #print(res)
-z=input('введи число: ')
-x=int(z)
-print(x+10)
+
+#z=input('введи число: ')
+#x=int(z)
+#print(x+10)
+
+z = int(input("введите число: "))
+print(z + 100)
+
+print(int(input("введите число: "))+100)
