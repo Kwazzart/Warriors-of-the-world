7ec6a28e02ce127f43d5117e94a993ae2f08558d
Shealtov <shealtov@gmail.com>
2021-07-21 12:39:27 +0300

    done.

--- Shealty/task 1.py
+++ Shealty/task 1.py
@@ -2,11 +2,6 @@
 #res=3**2
 #print(res)
 
-#z=input('введи число: ')
-#x=int(z)
-#print(x+10)
-
-z = int(input("введите число: "))
-print(z + 100)
-
-print(int(input("введите число: "))+100)
+z=input('введи число: ')
+if z.isdigit():
+    print(int(z)**2)
