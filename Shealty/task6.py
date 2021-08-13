5464df14ad74bc5c0892b5c04ba21468b4c8850c
Shealtov <shealtov@gmail.com>
2021-08-13 14:44:13 +0300

    ошибка

--- Shealty/task6.py
+++ Shealty/task6.py
@@ -2,6 +2,5 @@ x1 = int(input('enter x1: '))
 y1 = int(input('enter y1: '))
 x2 = int(input('enter x2: '))
 y2 = int(input('enter y2: '))
-distance = round((x1 - x2)**2 +
-(x2 - y2)**2, 3)
+distance = round((x1 - x2)**2 + (x2 - y2)**2, 3)
 print('distance between points is: ' + str(distance))
